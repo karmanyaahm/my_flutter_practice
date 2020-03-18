c790ca77dce58589ae8a54fa5c9cb7ceadea57f7
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-03-17 20:41:07 -0400

    went through step 2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,9 +5,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('hello'),
+        title: Text('Returning Data Demo'),
       ),
-      body: Text('hello'),
+      body: Center(
+        child: SelectionButton(),
+      ),
+    );
+  }
+}
+
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: Text('Pick an option, any option!'),
+    );
+  }
+
+  _navigateAndDisplaySelection(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SelectionScreen(),
+      ),
     );
   }
 }
